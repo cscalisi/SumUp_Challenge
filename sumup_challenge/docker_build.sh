90ddadd0b19a28e5d2c98f1724e511d2127f2a17
cscalisi <c.scalisi22@gmail.com>
2023-08-27 12:09:55 -0400

    Add docker files

---
+++ sumup_challenge/docker_build.sh
@@ -0,0 +1,15 @@
+docker build --tag my-dbt --target dbt-bigquery .
+
+docker run \
+  --mount type=bind,source=/Users/carloscalisi/Documents/SumUp_Challenge/sumup_challenge,target=/usr/app \
+  --mount type=bind,source=/Users/carloscalisi/Documents/SumUp_Challenge/sumup_challenge/profiles.yml,target=/root/.dbt/profiles.yml \
+  my-dbt \
+  docs generate
+
+
+docker run \
+  -p 8080:8080 \
+  --mount type=bind,source=/Users/carloscalisi/Documents/SumUp_Challenge/sumup_challenge,target=/usr/app \
+  --mount type=bind,source=/Users/carloscalisi/Documents/SumUp_Challenge/sumup_challenge/profiles.yml,target=/root/.dbt/profiles.yml \
+  my-dbt \
+  docs serve
