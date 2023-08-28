992c5a575c23308e807383e2ec9a06979e977d1c
cscalisi <c.scalisi22@gmail.com>
2023-08-27 23:55:09 -0400

    Remove venv

--- sumup_challenge/docker_build.sh
+++ sumup_challenge/docker_build.sh
@@ -1,11 +1,18 @@
 docker build --tag my-dbt --target dbt-bigquery .
 
 docker run \
+  --network=host \
+  --mount type=bind,source=/Users/carloscalisi/Documents/SumUp_Challenge/sumup_challenge,target=/usr/app \
+  --mount type=bind,source=/Users/carloscalisi/Documents/SumUp_Challenge/sumup_challenge/profiles.yml,target=/root/.dbt/profiles.yml \
+  my-dbt \
+  build
+
+docker run \
+  -p 8080:8080 \
   --mount type=bind,source=/Users/carloscalisi/Documents/SumUp_Challenge/sumup_challenge,target=/usr/app \
   --mount type=bind,source=/Users/carloscalisi/Documents/SumUp_Challenge/sumup_challenge/profiles.yml,target=/root/.dbt/profiles.yml \
   my-dbt \
   docs generate
-
 
 docker run \
   -p 8080:8080 \
